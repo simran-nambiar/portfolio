e4f99711ac1b70806dd7b2c67fb6b36f693a4506
simran-nambiar <soccerballer.nambiar@gmail.com>
2017-07-31 15:24:17 -0400

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,15 @@
+var greetingElement = document.getElementById("simran");
+
+// now we want to attach an event listener to this element
+// this is doing a few things:
+// 1. Add the event listener to the greetingElement
+// 2. We're "listening" for the 'click' event
+// 3. When the button gets clicked, the function will be called
+
+// so this is the general format (ps: there are more ways to do this!!!):
+// element.addEventListener("event", callback function)
+greetingElement.addEventListener("click", function(){
+  document.getElementById("simran").innerHTML = "you clicked me, thanks!";
+});
+
 
